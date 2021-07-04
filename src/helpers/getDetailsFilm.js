dcc849e659ce7157f5728886570b61cfcc94e953
danielcgilibert <danielcgilibert@gmail.com>
2021-07-04 23:38:47 +0200

    arreglado fallo http

--- src/helpers/getDetailsFilm.js
+++ src/helpers/getDetailsFilm.js
@@ -1,7 +1,7 @@
 
 export const getDetailsFilm = async(id) => {
     try {
-    const url = `http://www.omdbapi.com/?i=${id}&apikey=${process.env.REACT_APP_API_KEY}`
+    const url = `https://www.omdbapi.com/?i=${id}&apikey=${process.env.REACT_APP_API_KEY}`
     const resp = await fetch(url);
     const data = await resp.json();
 
