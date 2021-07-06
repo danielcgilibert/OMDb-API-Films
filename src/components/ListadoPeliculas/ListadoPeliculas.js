1ca2c9c7eb47c6568060717b491b35cd854366ec
danielcgilibert <danielcgilibert@gmail.com>
2021-07-06 02:38:47 +0200

    arreglado condicional page

--- src/components/ListadoPeliculas/ListadoPeliculas.js
+++ src/components/ListadoPeliculas/ListadoPeliculas.js
@@ -4,11 +4,14 @@ import { TarjetaPelicula } from "../TarjetaPelicula/TarjetaPelicula";
 export const ListadoPeliculas = ({ films, page }) => {
   return (
     <>
+    {films.length > 0 && (
       <div className="col-start-3">
-        <div class=" animate__animated animate__bounceInDown bg-white tracking-wide text-gray-800 font-bold rounded border-b-2 border-blue-500 hover:border-blue-600 hover:bg-blue-500 hover:text-white shadow-md py-2 px-6 inline-flex items-center">
-          Página {page}
-        </div>
+      <div class=" animate__animated animate__bounceInDown bg-white tracking-wide text-gray-800 font-bold rounded border-b-2 border-blue-500 hover:border-blue-600 hover:bg-blue-500 hover:text-white shadow-md py-2 px-6 inline-flex items-center">
+        Página {page}
       </div>
+    </div>
+    )}
+      
 
       <div className="col-span-3 ">
         {films.map((pelicula) => (
