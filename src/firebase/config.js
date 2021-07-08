b8b33d1bc6a3f442ea04164e903167702e239433
danielcgilibert <danielcgilibert@gmail.com>
2021-07-08 02:22:32 +0200

    borrado comentario

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,15 +2,7 @@ import firebase from "firebase/app";
 import 'firebase/firebase-firestore'
 import "firebase/auth";
 
-// const firebaseConfig = {
-//     apiKey: "AIzaSyCCWsPzxb2H7KzWL4r_wqFaXPSRnT16HHs",
-//     authDomain: "omdb-projecto.firebaseapp.com",
-//     projectId: "omdb-projecto",
-//     storageBucket: "omdb-projecto.appspot.com",
-//     messagingSenderId: "668355497102",
-//     appId: "1:668355497102:web:3250ab9feb6733f5d7cddf",
-//     measurementId: "G-Y28C0JWMLL"
-//   };
+
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_API_KEY_FIREBASE,
     authDomain: process.env.REACT_APP_authDomain_FIREBASE ,
